24107054a80346b674195ee69c0331c709d170b8
Bhargav Jaiswal <bhargavjaiswal24@gmail.com>
2016-04-24 23:41:47 -0700

    Added add, update delete functionalities

--- src/main/java/org/bhargav/messenger/resources/MessageResource.java
+++ src/main/java/org/bhargav/messenger/resources/MessageResource.java
@@ -2,7 +2,10 @@ package org.bhargav.messenger.resources;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -18,14 +21,28 @@ public class MessageResource {
 	
 	//mapped to class
 	@GET
-	@Produces(MediaType.APPLICATION_XML)
+	@Produces(MediaType.APPLICATION_JSON)
 	public List<message> getMessages(){
 		return messageservice.getAllMessages();
 	 }
 	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public message addMessage(message message){
+		return messageservice.addMessage(message);
+	}
+	
+	@PUT
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+		public message updateMessage(@PathParam("messageId") long id, message message){
+		message.setId(id);
+		return messageservice.updateMessage(message);
+	}
 	//map to method
 	@GET
-	@Produces(MediaType.APPLICATION_XML)
+	@Produces(MediaType.APPLICATION_JSON)
 	@Path("/{messageId}")
 	public message getMessage(@PathParam("messageId") long id){
 		return messageservice.getMessage(id);
